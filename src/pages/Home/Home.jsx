643ce165882f5ac876a49c1ece25ffc0cf50843e
Yan Martins Esteves <yan.esteves@audaces.com>
2023-07-03 21:18:37 -0300

    Aula 03/07/2023

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -11,7 +11,7 @@ function Home() {
     // condicao ? <VERDADEIRA> : <FALSA>
     const tituloNoticia = 'Grêmio vence América MG'
     const srcImage = "https://s2.glbimg.com/QxgfZZ0KaNp-BRgg_Pfile-2-WM=/0x468:1536x1332/fit-in/629x354/middle/smart/filters:strip_icc():strip_exif():format(webp)/i.s3.glbimg.com/v1/AUTH_bc8228b6673f488aa253bbcb03c80ec5/internal_photos/bs/2023/3/8/IFfzYCRkGLGL7jqP5YAg/fzqkz10wwaqujd6.jfif"
-
+      
     const handleComponenteFilho = (message) => {
         console.log('Mensagem do filho: ', message)
     }
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,15 +1,19 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import { useNavigate } from 'react-router-dom';
 import './styles.css'
 
 function Login() {
+
+    const passwordRef = useRef(null)
+
     const [email, setEmail] = useState()
     const [password, setPassword] = useState()
-    const [errorForm, setErrorForm] = useState(false) //
+
+    const [errorForm, setErrorForm] = useState(false)
     const navigate = useNavigate()
 
     useEffect(() => {
-        setErrorForm(false)     
+        setErrorForm(false)
     }, [password])
 
     /**
@@ -29,35 +33,42 @@ function Login() {
     }
 
     const handleSubmit = async (event) => {
-        event.preventDefault()       
-        console.log('Dados digitados:')   
+        event.preventDefault()
+        console.log('Dados digitados:')
         console.log('Email: ', email)
-        console.log('Password: ', password)  
+        console.log('Password: ', password)
         if (email === 'yan.m.esteves@gmail.com' && password === '12345678') {
             navigate('/')
         } else {
+            passwordRef.current.style.borderColor = 'red'
             setErrorForm(true)
-        }     
+        }
     }
 
-    return (                
-        <form onSubmit={handleSubmit}>
+    // const changeColor = () => {
+    //     console.log('MUDAR COR')
+    //     
+    // }
 
-            <h4>E-mail: {email}</h4>
+    return (
+        <>
+            <form style={{ 'maxWidth': '320px' }} onSubmit={handleSubmit}>
 
-            <input required type="email" value={email} 
-                placeholder="Digite seu e-mail"
-                onChange={handleEmailChange} />
+                <input required type="email" value={email}
+                    placeholder="Digite seu e-mail"
+                    onChange={handleEmailChange} />
 
-            <input required type="password" minLength="6" 
-                value={password} 
-                placeholder="Digite sua senha" 
-                onChange={handlePasswordChange}/>
-            
-            {errorForm && <span>E-mail e/ou senha inválidos</span>}
+                <input required type="password" minLength="6"
+                    value={password} ref={passwordRef} placeholder="Digite sua senha"
+                    onChange={handlePasswordChange} />
 
-            <button type="submit">Entrar</button>
-        </form>
+                {errorForm && <span>E-mail e/ou senha inválidos</span>}
+
+                <button type="submit">Entrar</button>
+            </form>
+
+            {/* <button onClick={changeColor}>Mudar cor</button> */}
+        </>
     )
 }
 
