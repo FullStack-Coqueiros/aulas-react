3b439acafbc94e37854ec3ca527fb6b9df7972ad
Yan Martins Esteves <yan.esteves@audaces.com>
2023-07-04 21:27:58 -0300

    Aula 04/07/2023

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,55 +1,48 @@
 import { useNavigate } from "react-router-dom";
-
 import CardNoticia from "../../components/HomeComponents/CardNoticia/CardNoticia"
-import ComponenteFilho from "../../components/HomeComponents/ComponenteFilho/ComponenteFilho"
+import { useState, useEffect } from "react";
 import { getAll } from "../../services/web";
 
 function Home() {
-    getAll()
+    const [noticias, setNoticias] = useState([])
+    
+    useEffect(() => {
+        async function fetchAll() {
+            const data = await getAll()
+            setNoticias(data)
+        }
+        
+        fetchAll()
 
-    const navigate = useNavigate()
+    }, [])
 
     const temNoticia = true;
-    // condicao ? <VERDADEIRA> : <FALSA>
-    const tituloNoticia = 'Grêmio vence América MG'
-    const srcImage = "https://s2.glbimg.com/QxgfZZ0KaNp-BRgg_Pfile-2-WM=/0x468:1536x1332/fit-in/629x354/middle/smart/filters:strip_icc():strip_exif():format(webp)/i.s3.glbimg.com/v1/AUTH_bc8228b6673f488aa253bbcb03c80ec5/internal_photos/bs/2023/3/8/IFfzYCRkGLGL7jqP5YAg/fzqkz10wwaqujd6.jfif"
       
-    const handleComponenteFilho = (message) => {
-        console.log('Mensagem do filho: ', message)
-    }
-
     const renderizarQtdNoticias = () => {
         if (temNoticia) {
-            return <span>Tem 1 notícia não lida</span>
+            return <span>Tem {noticias.length} notícias não lidas</span>
         } 
         return <span>Não existe notícia.</span>
-    }
-    
-
-    const handleClick = () => {
-        navigate('/empresa/senai')
-    }
-
-    const handleClick2 = () => {
-        navigate('/empresa/audaces')
     }
     
     return (
         <>
             <h1>Home</h1>
-
-            <button onClick={handleClick}>Ver Senai</button>
-            <button onClick={handleClick2}>Ver Audaces</button>
-
+            
             {renderizarQtdNoticias()}
 
-            {temNoticia ? (
+            {noticias.map(noticia => {
+                return <CardNoticia key={noticia.id} 
+                            titulo={noticia.titulo} srcImage={noticia.imagem.src} />
+            })}
+
+            {/* {temNoticia ? (
                 <CardNoticia
                     titulo={tituloNoticia}
                     srcImage={srcImage} />
             ) : (
                 <span>Não existem notícias.</span>
-            )}
+            )} */}
 
 
             {/* {temNoticia && 
@@ -62,7 +55,7 @@ function Home() {
                 titulo={tituloNoticia}
                 srcImage={srcImage} /> */}
 
-            <ComponenteFilho callback={handleComponenteFilho} />
+            {/* <ComponenteFilho callback={handleComponenteFilho} /> */}
 
         </>
     )
--- src/services/web.js
+++ src/services/web.js
@@ -3,7 +3,5 @@ const URL_API = 'http://localhost:3000'
 export async function getAll() {
     const response = await fetch(`${URL_API}/noticias`)
     const formatado = await response.json()
-    console.log('-- resposta --')
-    console.log(formatado)
     return formatado
 }
